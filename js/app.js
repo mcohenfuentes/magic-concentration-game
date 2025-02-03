b4dd253ea5793a5925d0e4b30346eb47c747cb37
Mica CF <mcohenfuentes@gmail.com>
2025-02-03 12:54:56 -0500

    can select the cards individually

--- js/app.js
+++ js/app.js
@@ -1,43 +1,52 @@
 /*-------------------------------- Constants --------------------------------*/
 
-// Define constant for cards array 
-// is my array constant or does it change when I shuffle the cards? Will I need two arrays cards vs. shuffled cards?
+const cards = ['✨', '🪄', '🔮', '🦄','✨', '🪄', '🔮', '🦄']
 // Define constant for cards total?
 // Define constant for total pairs 
 // const gameBoard?
 
 /*---------------------------- Variables (state) ----------------------------*/
-
+let cardsTotal= 8
 let firstCard
 let secondCard
 let hasFlippedCard = false 
-when turn is over count = 4
-if matching pairs = 2 user wins
-if matching pairs < 2 user loses 
-let totalCards = 8 would I need this variable for anything?
-let totalPairs = 4 would I need this variable for anything?
-let delay flips cards back over after 3 seconds when cards don't match. Matching pairs is reset to 0.
-let message
+correctCards = 0
 
-// what do you do if you click card twice in one round so user can't
-click on same card twice
+
 
 /*------------------------ Cached Element References ------------------------*/
 
-Define cached for cards
-Define cached for win/lose message
-Define cached for replay button
+const firstUnicorn = document.getElementById('firstUnicorn')
+
+
+const unicorn = document.getElementById('unicorn')
+
+
+const firstSparkles = document.getElementById('firstSparkles')
+
+
+const sparkles = document.getElementById('sparkles')
+
+
+const firstFortune = document.getElementById('firstFortune')
+
+
+const fortune = document.getElementById('fortune')
+
+
+const firstWand = document.getElementById('firstWand')
+
+
+const wand = document.getElementById('wand')
+
+const messageEl = document.querySelector('#message')
+
+const resetBtnEl = document.querySelector('#replay')
 
 /*-------------------------------- Functions --------------------------------*/
-flip card function = card is flipped. Emoji is made visible
-match function = card is flipped. If emojis match. Match = true and leave cards visible. If emojis don't match. Match = false 
-and cards turn back over
 
-functions for the different pairs?
 
 /*----------------------------- Event Listeners -----------------------------*/
 
-// Add event listener for every card when it's clicked and then card flips?
-// 
-// Add event listener for the reset button 
+firstUnicorn.addEventListener('click', firstUnicornClick)
 
