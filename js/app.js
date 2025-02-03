a4ff3718fe5a33a93ff0f539f9c0a17edd4ddad9
Mica CF <mcohenfuentes@gmail.com>
2025-02-03 10:53:31 -0500

    saved header

---
+++ js/app.js
@@ -0,0 +1,43 @@
+/*-------------------------------- Constants --------------------------------*/
+
+// Define constant for cards array 
+// is my array constant or does it change when I shuffle the cards? Will I need two arrays cards vs. shuffled cards?
+// Define constant for cards total?
+// Define constant for total pairs 
+// const gameBoard?
+
+/*---------------------------- Variables (state) ----------------------------*/
+
+let firstCard
+let secondCard
+let hasFlippedCard = false 
+when turn is over count = 4
+if matching pairs = 2 user wins
+if matching pairs < 2 user loses 
+let totalCards = 8 would I need this variable for anything?
+let totalPairs = 4 would I need this variable for anything?
+let delay flips cards back over after 3 seconds when cards don't match. Matching pairs is reset to 0.
+let message
+
+// what do you do if you click card twice in one round so user can't
+click on same card twice
+
+/*------------------------ Cached Element References ------------------------*/
+
+Define cached for cards
+Define cached for win/lose message
+Define cached for replay button
+
+/*-------------------------------- Functions --------------------------------*/
+flip card function = card is flipped. Emoji is made visible
+match function = card is flipped. If emojis match. Match = true and leave cards visible. If emojis don't match. Match = false 
+and cards turn back over
+
+functions for the different pairs?
+
+/*----------------------------- Event Listeners -----------------------------*/
+
+// Add event listener for every card when it's clicked and then card flips?
+// 
+// Add event listener for the reset button 
+
