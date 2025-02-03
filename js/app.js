80891e5cac5ec7d239e1020306b42a8a09e9ea93
Mica CF <mcohenfuentes@gmail.com>
2025-02-03 17:00:06 -0500

    added event listener

--- js/app.js
+++ js/app.js
@@ -1,52 +1,42 @@
 /*-------------------------------- Constants --------------------------------*/
 
 const cards = ['✨', '🪄', '🔮', '🦄','✨', '🪄', '🔮', '🦄']
-// Define constant for cards total?
-// Define constant for total pairs 
-// const gameBoard?
+
 
 /*---------------------------- Variables (state) ----------------------------*/
-let cardsTotal= 8
-let firstCard
-let secondCard
-let hasFlippedCard = false 
-correctCards = 0
+
+let firstCard = ''
+let secondCard = ''
+let matchedPairs = 0 
+
 
 
 
 /*------------------------ Cached Element References ------------------------*/
 
-const firstUnicorn = document.getElementById('firstUnicorn')
-
-
-const unicorn = document.getElementById('unicorn')
-
-
-const firstSparkles = document.getElementById('firstSparkles')
-
-
-const sparkles = document.getElementById('sparkles')
-
-
-const firstFortune = document.getElementById('firstFortune')
-
-
-const fortune = document.getElementById('fortune')
-
-
-const firstWand = document.getElementById('firstWand')
-
-
-const wand = document.getElementById('wand')
+const cardEl = document.querySelectorAll('.card')
 
 const messageEl = document.querySelector('#message')
 
-const resetBtnEl = document.querySelector('#replay')
+const replayBtnEl = document.querySelector('#replay')
+
 
 /*-------------------------------- Functions --------------------------------*/
 
+const startGame = () => {
+
+}
+
+function handleClick(event) {
+   
+
+}
 
 /*----------------------------- Event Listeners -----------------------------*/
 
-firstUnicorn.addEventListener('click', firstUnicornClick)
+cardEl.forEach(card => {
+    card.addEventListener('click', handleClick);
+});
+
+
 
