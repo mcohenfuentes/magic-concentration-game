f32877c8eb8ada82460ed4efdfe9660f04df972a
Mica CF <mcohenfuentes@gmail.com>
2025-02-04 10:13:18 -0500

    additions to handleClick

--- js/app.js
+++ js/app.js
@@ -1,15 +1,15 @@
 /*-------------------------------- Constants --------------------------------*/
 
-const cards = ['✨', '🪄', '🔮', '🦄','✨', '🪄', '🔮', '🦄']
+const cards = ['✨', '🪄', '🔮', '🦄', '✨', '🪄', '🔮', '🦄']
 
 
 /*---------------------------- Variables (state) ----------------------------*/
 
-let firstCard = ''
-let secondCard = ''
-let matchedPairs = 0 
-
-
+let firstCard = '' 
+let secondCard = '' 
+let matchedPairs = 0
+let round = 0
+let clicks = 0
 
 
 /*------------------------ Cached Element References ------------------------*/
@@ -28,15 +28,57 @@ const startGame = () => {
 }
 
 function handleClick(event) {
-   
+    let clickedCard = event.target
+    
+    // if clicks are odd the user is still in a round
+    // if clicks are even the user has completed a round
 
+    clicks += 1
+    console.log(clicks)
+
+    if (clicks % 2 == 0) {
+        round++
+    } 
+    console.log(round)
+
+    if (!firstCard) {
+        firstCard = clickedCard
+        console.log('first card', firstCard.innerText)
+        
+    } else if (!secondCard) {
+        secondCard = clickedCard
+        console.log('second card', secondCard.innerText)
+        round += 1
+        console.log(round)
+    }
+
+    if (firstCard.innerText === secondCard.innerText) {
+        console.log('match!')
+    } else {
+        console.log('not a match')
+    }
+    if (round = 6 && matchedPairs != 4) {
+        console.log('lose')
+    }
+        
 }
+// how to bridge btw rounds and matches?
+//     check to see if both cards match first card === second card
+// if they do match cards stay facing up and set first card and second 
+// card to null again 
+// if they dont match cards flip back over
+// after click o first card and 
+
+// if (!firstCard) {
+//     firstCard = null
+//     console.log('first card', firstCard.innerText)
+// } else {
+//     secondCard = null
+//     console.log('second card', secondCard.innerText)
+// }
 
 /*----------------------------- Event Listeners -----------------------------*/
 
 cardEl.forEach(card => {
     card.addEventListener('click', handleClick);
 });
-
-
-
