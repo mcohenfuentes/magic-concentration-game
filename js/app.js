1ec4d2f9d174bc9d2d9a0d8f7ed52a40fe08237b
Mica CF <mcohenfuentes@gmail.com>
2025-02-04 16:09:02 -0500

    added toggle events

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@ const cards = ['✨', '🪄', '🔮', '🦄', '✨', '🪄', '🔮', '🦄']
 /*---------------------------- Variables (state) ----------------------------*/
 
 let firstCard = null
-let secondCard = null 
+let secondCard = null
 let matchedPairs = 0
 let round = 0
 let clicks = 0
@@ -14,7 +14,7 @@ let clicks = 0
 
 /*------------------------ Cached Element References ------------------------*/
 
-const cardEl = document.querySelectorAll('.card')
+const cardEls = document.querySelectorAll('.card')
 
 const messageEl = document.querySelector('#message')
 
@@ -23,13 +23,32 @@ const replayBtnEl = document.querySelector('#replay')
 
 /*-------------------------------- Functions --------------------------------*/
 
-function init() {
-    firstCard = null
-    secondCard = null
-    round = 0
-    clicks = 0
-}
-        
+// function init() {
+//     firstCard = null
+//     secondCard = null
+//     round = 0
+//     clicks = 0
+// }
+cardEls.forEach(function (card) {
+    if (card.classList.contains('unicorn')) {
+        card.addEventListener('click', function(event){
+            event.target.classList.toggle('unicorn')
+        })
+    } else if (card.classList.contains('sparkles')) {
+        card.addEventListener('click', function(event){
+            event.target.classList.toggle('sparkles')
+        })
+    } else if (card.classList.contains('fortune')) {
+        card.addEventListener('click', function(event){
+            event.target.classList.toggle('fortune')
+        })
+    } else if (card.classList.contains('wand')) {
+        card.addEventListener('click', function(event){
+            event.target.classList.toggle('wand')
+        })
+    }
+}) 
+
 
 function handleClick(event) {
     let clickedCard = event.target
@@ -40,7 +59,7 @@ function handleClick(event) {
     if (!firstCard) {
         firstCard = clickedCard
         console.log('first card', firstCard.innerText)
-        
+
     } else if (!secondCard) {
         secondCard = clickedCard
         console.log('second card', secondCard.innerText)
@@ -50,30 +69,38 @@ function handleClick(event) {
 
     if (firstCard.innerText === secondCard.innerText) {
         console.log('match!')
+
+        firstCard.removeEventListener('click', handleClick)
+        secondCard.removeEventListener('click', handleClick)
+
         matchedPairs += 1
-        
+
     } else {
         console.log(firstCard.innerText)
         console.log(secondCard.innerText)
         console.log('not a match')
+
+        firstCard.removeEventListener('click', handleClick)
+        secondCard.removeEventListener('click', handleClick)
+
     }
 
     resetTurn()
-    
+
 
     if (matchedPairs === 4) {
         console.log('win!')
     }
     else if (round === 5 && matchedPairs !== 4) {
         console.log('lose!')
-    
-        
-    }     
-}
-    const resetTurn = () => {
-        firstCard = null
-        secondCard = null
+
+
     }
+}
+const resetTurn = () => {
+    firstCard = null
+    secondCard = null
+}
 
 
 // how to bridge btw rounds and matches?
@@ -87,6 +114,6 @@ function handleClick(event) {
 
 /*----------------------------- Event Listeners -----------------------------*/
 
-cardEl.forEach(card => {
+cardEls.forEach(card => {
     card.addEventListener('click', handleClick);
 });
